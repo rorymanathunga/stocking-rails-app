c0ba064c54af5a5486e842a59cceb66e6f9a4525
Rory Manathunga <thedarkknight@Rorys-MacBook-Pro-2.local>
2019-10-17 13:31:09 -0700

    Added stock quote gem

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @client = IEX::Api::Client.new(
+      publishable_token: 'Tpk_ccf8b92f0f0b405bbb17b313af8da443',
+      endpoint: 'https://sandbox.iexapis.com/v1'
+    )
+    if params[:ticker]
+      @stock = @client.key_stats(params[:ticker])
+    end
+
   end
   def about
   end
---
+++ vendor/bundle/gems/iex-ruby-client-1.1.0/lib/iex/version.rb
@@ -0,0 +1,3 @@
+module IEX
+  VERSION = '1.1.0'.freeze
+end
