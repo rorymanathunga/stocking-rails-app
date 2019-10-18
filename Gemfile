3295eb83c577766eeb3e424739f8e4700d2970e6
Rory Manathunga <thedarkknight@Rorys-MacBook-Pro-2.local>
2019-10-18 12:53:08 -0700

    Added jwt gem

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootsnap', '>= 1.4.2', require: false
 gem 'iex-ruby-client'
 gem 'devise', '~> 4.7', '>= 4.7.1'
+gem 'jwt', '~> 2.2', '>= 2.2.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
---
+++ app/controllers/thinkific_controller.rb
@@ -0,0 +1,44 @@
+require 'securerandom' unless defined?(SecureRandom)
+
+class SessionController < ApplicationController
+  # Configuration
+  THINKIFIC_API_KEY = ENV["THINKIFIC_API_KEY"]
+  THINKIFIC_SUBDOMAIN = ENV["THINKIFIC_SUBDOMAIN"]
+
+  def create
+    if user = User.authenticate(params[:login], params[:password])
+      # If the submitted credentials pass, then log user into Thinkific
+      sign_into_thinkific(user)
+    else
+      render :new, :notice => "Invalid credentials"
+    end
+  end
+
+  private
+
+  def sign_into_thinkific(user)
+    # This is the meat of the business, set up the parameters you wish
+    # to forward to Thinkific. All parameters are documented in this page.
+
+
+    iat = Time.now.to_i
+    jti = "#{iat}/#{SecureRandom.hex(18)}"
+    payload = JWT.encode({
+      :iat   => iat,
+      :jti   => jti,
+      :first_name  => user.first_name,
+      :last_name => user.last_name,
+      :email => user.email,
+     }, THINKIFIC_API_KEY)
+    redirect_to thinkific_sso_url(payload)
+  end
+
+
+  def thinkific_sso_url(payload)
+    url = "http://#{THINKIFIC_SUBDOMAIN}.thinkific.com/api/sso/v2/sso/jwt?jwt=#{payload}"
+    url += "&return_to=#{URI.escape(params["return_to"])}" if params["return_to"].present?
+    url += "&error_url=#{URI.escape(params["error_url"])}" if params["error_url"].present?
+    url
+  end
+
+end
